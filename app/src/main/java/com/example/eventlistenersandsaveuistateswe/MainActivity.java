59646b401d2864e127c5a4a62df521cce68b8d53
Royal Dougles <maddysachin786@gmail.com>
2021-06-29 23:29:33 +0530

    Initial state

---
+++ app/src/main/java/com/example/eventlistenersandsaveuistateswe/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.eventlistenersandsaveuistateswe;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView=findViewById(R.id.tvSaveInstance);
+        textView.setText("Welcome to Masai School");
+        if(savedInstanceState!=null){
+            String data=savedInstanceState.getString("key");
+            textView.setText(data);
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull  Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("key","Screen Rotated");
+    }
+
+
+}
